9657c806c9f13ba6a742e57891e2889d7236aa08
RayonB <rinevarayon@gmail.com>
2024-05-12 14:42:10 +0800

    dashboard

--- public/user/dashboard.php
+++ public/user/dashboard.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Rayonskie</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -31,7 +31,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <h1 class="my-5">Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the site.</h1>
     <p>
         <a href="./reset.php" class="btn btn-warning">Reset Your Password</a>
         <a href="./logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
@@ -60,7 +60,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                                             <th>#</th>
                                             <th>Product Name</th>
                                             <th>Product Description</th>
-                                            <th>Retail Price</th>
+                                            <th>Product Retail Price</th>
+                                            <th>Product Date Added</th>
+                                            <th>Product Updated Date</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
@@ -77,7 +79,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                                     <td>{{product_name}}</td>
                                     <td>{{product_details}}</td>
                                     <td>{{product_retail_price}}</td>
-                                    <td>
+                                    <td>{{product_date_added}}</td>
+                                    <td>{{product_updated_date}}</td>
+                                <td>
                                         <a href="../inventory/read.php?product_id={{product_id}}" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
                                         <a href="../inventory/update.php?product_id={{product_id}}" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
                                         <a href="../inventory/delete.php?product_id={{product_id}}" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
@@ -89,8 +93,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                             $rows = '';
                             while ($row = $result->fetch()) {
                                 $rowHtml = str_replace(
-                                    array('{{product_id}}', '{{product_name}}', '{{product_details}}', '{{product_retail_price}}'),
-                                    array($row['product_id'], $row['product_name'], $row['product_details'], $row['product_retail_price']),
+                                    array('{{product_id}}', '{{product_name}}', '{{product_details}}', '{{product_retail_price}}', '{{product_date_added}}', '{{product_updated_date}}'),
+                                    array($row['product_id'], $row['product_name'], $row['product_details'], $row['product_retail_price'], $row['product_date_added'], $row['product_updated_date']),
                                     $rowTemplate
                                 );
                                 $rows .= $rowHtml;
