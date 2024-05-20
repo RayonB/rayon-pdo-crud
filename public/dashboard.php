9aa83286c5843a1ecdce7de78f9cbc6ea6f2864b
RayonB <rinevarayon@gmail.com>
2024-05-20 10:54:42 +0800

    editing dashboard

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,18 +4,16 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ../../index.php");
+    header("location: ../index.php");
     exit;
 }
 ?>
- 
-
 
  <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Rayonskie</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -53,94 +51,69 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         }
     </style>
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand"  href="dashboard.php">Admin</a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">Dashboard <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="products.php">Products</a>
+      </li>
+    </ul>
+    
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+     <!--<a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>-->
+        <a href="../public/user/logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </form>
+  </div>
+</nav>
+
 <body>
-    <h1 class="my-5">Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the site.</h1>
-    <p>
-        <a href="./reset.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="./logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Product Details</h2>
-                        <a href="../inventory/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Product</a>
-                    </div>
-                    <?php
-                    // Include config file
-                    require_once "../../db/config.php";
+    
+<div class="container-fluid" >
+    <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header" href="products.php">Products Recorded</div>
+        <div class="card-body">
+            <h5 class="card-title "></h5>
+
+            <?php
+            // Include config file
+            require_once "../db/config.php";
+            
+            // Attempt select query execution
+            $sql = "SELECT COUNT(*) AS total_products FROM products";
+            if($stmt = $pdo->prepare($sql)){
+                // Attempt to execute the prepared statement
+                if($stmt->execute()){
+                    // Bind result variables
+                    $stmt->bindColumn('total_products', $total_products);
                     
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM products";
-                    if($result = $pdo->query($sql)){
-                        if($result->rowCount() > 0){
-                            // Define the table template
-                            $tableTemplate = '
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Product Name</th>
-                                            <th>Product Description</th>
-                                            <th>Product Retail Price</th>
-                                            <th>Product Date Added</th>
-                                            <th>Product Updated Date</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {{rows}}
-                                    </tbody>
-                                </table>
-                            ';
-                    
-                            // Define the row template
-                            $rowTemplate = '
-                                <tr>
-                                    <td>{{product_id}}</td>
-                                    <td>{{product_name}}</td>
-                                    <td>{{product_details}}</td>
-                                    <td>{{product_retail_price}}</td>
-                                    <td>{{product_date_added}}</td>
-                                    <td>{{product_updated_date}}</td>
-                                    <td>
-                                        <a href="../inventory/read.php?product_id={{product_id}}" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
-                                        <a href="../inventory/update.php?product_id={{product_id}}" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
-                                        <a href="../inventory/delete.php?product_id={{product_id}}" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
-                                    </td>
-                                </tr>
-                            ';
-                    
-                            // Populate the rows using the row template
-                            $rows = '';
-                            while ($row = $result->fetch()) {
-                                $rowHtml = str_replace(
-                                    array('{{product_id}}', '{{product_name}}', '{{product_details}}', '{{product_retail_price}}', '{{product_date_added}}', '{{product_updated_date}}'),
-                                    array($row['product_id'], $row['product_name'], $row['product_details'], $row['product_retail_price'], $row['product_date_added'], $row['product_updated_date']),
-                                    $rowTemplate
-                                );
-                                $rows .= $rowHtml;
-                            }
-                    
-                            // Replace the rows placeholder in the table template with the actual rows
-                            echo str_replace('{{rows}}', $rows, $tableTemplate);
-                            
-                            // Free result set
-                            unset($result);
-                        } else{
-                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-                        }
+                    // Fetch result
+                    if($stmt->fetch()){
+                        echo '<h1 class="text-center">' . $total_products . '</h1><br>';
                     } else{
-                        echo "Oops! Something went wrong. Please try again later.";
+                        echo '<p>No products found.</p>';
                     }
-                    
-                    // Close connection
-                    unset($pdo);
-                    ?>
-                </div>
-            </div>        
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+            }
+            // Close statement
+            unset($stmt);
+            ?>
+            
+            <p class="card-text">Date and Time: <?php echo date("Y-m-d H:i:s"); ?></p>
         </div>
     </div>
+</div>
 </body>
 </html>
