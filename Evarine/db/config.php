557b0010d714092dcb3198001012629a586a0357
rayon <rajnxjbx@gmail.com>
2024-05-22 13:32:57 +0800

    added initials

--- Evarine/db/config.php
+++ Evarine/db/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'Rayon');
+define('DB_NAME', 'qtest');
 
 /* Attempt to connect to MySQL database */
 try{
